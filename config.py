9a63245f92ff54b1796dad4497004dd9804d15d6
GMGayushdev20099 <findingtogamer@gmail.com>
2024-08-02 12:17:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = 
-API_HASH = ""
-BOT_TOKEN = ""
-MONGO_DB_URI = ""
+API_ID = 20121020 
+API_HASH = "515c45f734314c373d7e3e16b00c2c43"
+BOT_TOKEN = "7220778996:AAHh2wiXUOf-0vCeGp6LBx5lIk6KA-mH7kU"
+MONGO_DB_URI = "mongodb+srv://sorrythe925:sorrythe925@cluster0.jufq0io.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
-LOG_GROUP_ID = 
-OWNER_ID = 
+LOG_GROUP_ID = -1002019748324 
+OWNER_ID = 6489292415
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -29,8 +29,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/craftland209")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/craftland209")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -52,7 +52,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = ""
+STRING1 = "BQEzBbwAQWblG86J5x9pLNlwtoxQ_385YSv7dxCxmxVe5i2L6mQGaNDSL3LXMoUlU0cWi5aorNrRYOhAtptvZLtuFJUB89gfeljaJBCNcks-OjK_zmfLEHuqicjrCqfzPb4ba2MDV4KGQKWPa_6MCJC0Gmior0xhgfmp899Dz-1oeu4VWIFoLyG3-Y13oqUCrRvQZnBgn9TfcLxkoGHlI6rJ0u4bBpKEFUlfJEt6fjVOJJ3dujxMZe1-n1WIhpZ-6fVcM75pPfJJVgPI2UBr-oIgDdcOOdL4ZSWNpI3q4U5KVh_HXqy16nplVXtf8xlmqbru05QOXpdUrnP3Ke8jWRaHkWkYYAAAAAGCyr5_AA"
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -68,10 +68,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/d0e6aa5fc70d022a4f312.jpg"
+    "START_IMG_URL", "https://graph.org/file/96fb65671672cc24c35a9.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/d0e6aa5fc70d022a4f312.jpg"
+    "PING_IMG_URL", "https://graph.org/file/96fb65671672cc24c35a9.jpg"
 )
 PLAYLIST_IMG_URL = "https://graph.org/file/7aa263a19bd1b8148411b.jpg"
 STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
